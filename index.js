ebb7aad44295a51ec9fe1a5ddb733f5158d9b6dd
joel <joelbitar@protonmail.com>
2017-04-19 20:22:08 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function iterativeLog(array) {
+  array.forEach((element, index, array) => {
+    console.log(`${index}: ${element}`)
+  })
+}
+
+function iterate(callback) {
+  var odds = [1,3,5,7,9];
+
+  odds.forEach(callback);
+  return odds;
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback);
+}
