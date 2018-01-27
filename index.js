1f0c32775c9832ae71ef1a9c2470979887b9e76f
Michael Wahlin <wahlin@gmail.com>
2018-01-27 14:14:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 function iterativeLog(array) {
-  array.forEach((element, index) => console.log(`${index}: ${element}`))
+  array.forEach((element, index) => console.log(`${index}: ${element}`));
+}
+
+function iterate(callback) {
+  array = ['candy', 'corn'];
+  array.forEach(callback);
+  return array
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback)
 }
